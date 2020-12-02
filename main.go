463abedf01eb1bdccec8f1431c55033c8838cdc6
Rob Streeting <robsws@gmail.com>
2020-12-02 10:13:23 +0000

    Day two solution

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"aoc-2020/files"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 )
 
 func main() {
 	filename := os.Args[1]
-	dayoneparttwo(filename)
+	daytwoparttwo(filename)
 }
 
 func dayonepartone(filename string) {
@@ -41,4 +43,48 @@ func dayoneparttwo(filename string) {
 		}
 		numbers = append(numbers, p)
 	}
+}
+
+func daytwopartone(filename string) {
+	fileStream := make(chan string)
+	go files.StreamLines(filename, fileStream)
+	re := regexp.MustCompile("^([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)$")
+	valids := 0
+	for line := range fileStream {
+		submatches := re.FindStringSubmatch(line)
+		min, _ := strconv.Atoi(submatches[1])
+		max, _ := strconv.Atoi(submatches[2])
+		rulechar := []rune(submatches[3])[0]
+		password := submatches[4]
+		charcount := 0
+		for _, char := range password {
+			if char == rulechar {
+				charcount++
+			}
+		}
+		if charcount >= min && charcount <= max {
+			valids++
+		}
+	}
+	fmt.Println(valids)
+}
+
+func daytwoparttwo(filename string) {
+	fileStream := make(chan string)
+	go files.StreamLines(filename, fileStream)
+	re := regexp.MustCompile("^([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)$")
+	valids := 0
+	for line := range fileStream {
+		submatches := re.FindStringSubmatch(line)
+		loc1, _ := strconv.Atoi(submatches[1])
+		loc2, _ := strconv.Atoi(submatches[2])
+		loc1--
+		loc2--
+		rulechar := []rune(submatches[3])[0]
+		password := submatches[4]
+		if (rune(password[loc1]) == rulechar || rune(password[loc2]) == rulechar) && !(rune(password[loc1]) == rulechar && rune(password[loc2]) == rulechar) {
+			valids++
+		}
+	}
+	fmt.Println(valids)
 }
