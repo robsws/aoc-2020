a5ba1b162a83aa8e509af1a205b2e2bbbbb44a51
Rob Streeting <robsws@gmail.com>
2020-12-04 12:58:29 +0000

    day four solution

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	filename := os.Args[1]
-	daythreepartone(filename)
+	dayfourparttwo(filename)
 }
 
 func dayonepartone(filename string) {
@@ -126,4 +126,126 @@ func countTrees(filename string, dx int, dy int, resultStream chan int) {
 		y++
 	}
 	resultStream <- trees
+}
+
+func dayfourpartone(filename string) {
+	fileStream := make(chan string)
+	go files.StreamLines(filename, fileStream)
+	valids := 0
+	re := regexp.MustCompile("([a-z]{3}):[^ ]+")
+	currentFields := make(map[string]bool)
+	for line := range fileStream {
+		if line == "" {
+			if validatePassportFields(currentFields) {
+				valids++
+			}
+			currentFields = make(map[string]bool)
+		} else {
+			fields := re.FindAllStringSubmatch(line, -1)
+			for _, field := range fields {
+				currentFields[field[1]] = true
+			}
+		}
+	}
+	if validatePassportFields(currentFields) {
+		valids++
+	}
+	fmt.Println(valids)
+}
+
+func validatePassportFields(fields map[string]bool) bool {
+	requiredFields := [7]string{"byr", "ecl", "eyr", "hcl", "hgt", "iyr", "pid"}
+	valid := true
+	for _, key := range requiredFields {
+		_, exists := fields[key]
+		if !exists {
+			valid = false
+			break
+		}
+	}
+	return valid
+}
+
+func dayfourparttwo(filename string) {
+	fileStream := make(chan string)
+	go files.StreamLines(filename, fileStream)
+	valids := 0
+	re := regexp.MustCompile("([a-z]{3}):([^ ]+)")
+	currentFields := make(map[string]string)
+	for line := range fileStream {
+		if line == "" {
+			if validatePassport(currentFields) {
+				valids++
+			}
+			currentFields = make(map[string]string)
+		} else {
+			fields := re.FindAllStringSubmatch(line, -1)
+			for _, field := range fields {
+				currentFields[field[1]] = field[2]
+			}
+		}
+	}
+	if validatePassport(currentFields) {
+		valids++
+	}
+	fmt.Println(valids)
+}
+
+func validatePassport(fields map[string]string) bool {
+	requiredFields := map[string]func(string) bool{
+		"byr": func(v string) bool { return validateYear(v, 1920, 2002) },
+		"ecl": validateEyeColour,
+		"eyr": func(v string) bool { return validateYear(v, 2020, 2030) },
+		"hcl": validateHairColour,
+		"hgt": validateHeight,
+		"iyr": func(v string) bool { return validateYear(v, 2010, 2020) },
+		"pid": validatePassportNumber,
+	}
+	valid := true
+	for key, validator := range requiredFields {
+		_, exists := fields[key]
+		if !exists {
+			valid = false
+		} else {
+			valid = validator(fields[key])
+		}
+		if !valid {
+			break
+		}
+	}
+	return valid
+}
+
+func validateYear(year string, min, max int) bool {
+	yearI, _ := strconv.Atoi(year)
+	return yearI <= max && yearI >= min
+}
+
+func validateHeight(h string) bool {
+	re := regexp.MustCompile("([0-9]+)(cm|in)")
+	if !re.MatchString(h) {
+		return false
+	}
+	submatches := re.FindStringSubmatch(h)
+	height, _ := strconv.Atoi(submatches[1])
+	if submatches[2] == "cm" {
+		return height >= 150 && height <= 193
+	} else {
+		return height >= 59 && height <= 76
+	}
+}
+
+func validateHairColour(c string) bool {
+	re := regexp.MustCompile("^#[0-9a-f]{6}$")
+	return re.MatchString(c)
+}
+
+func validateEyeColour(c string) bool {
+	re := regexp.MustCompile("^amb|blu|brn|gry|grn|hzl|oth$")
+	return re.MatchString(c)
+}
+
+func validatePassportNumber(n string) bool {
+	re := regexp.MustCompile("^[0-9]{9}$")
+	return re.MatchString(n)
 }
---
+++ utils/maps.go
@@ -0,0 +1,12 @@
+package utils
+
+// Keys - Get all keys of a map
+func Keys(m map[string]interface{}) []string {
+	keys := make([]string, len(m))
+	i := 0
+	for k := range m {
+		keys[i] = k
+		i++
+	}
+	return keys
+}
