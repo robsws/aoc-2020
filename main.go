d2d22ba79e221fdbae8139348f68842fdfb1bd9d
Rob Streeting <robsws@gmail.com>
2020-12-06 17:26:02 +0000

    day six solutions

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	filename := os.Args[1]
-	dayfiveparttwo(filename)
+	daysixparttwo(filename)
 }
 
 func dayonepartone(filename string) {
@@ -343,4 +343,57 @@ func parseColumnLocation(colstr string) int {
 		poweroftwo *= 2
 	}
 	return col
+}
+
+func daysixpartone(filename string) {
+	fileStream := make(chan string)
+	go files.StreamLines(filename, fileStream)
+	total := 0
+	group := make(map[rune]int)
+	for line := range fileStream {
+		if line == "" {
+			for _, n := range group {
+				total += n
+			}
+			group = make(map[rune]int)
+			continue
+		}
+		for i := 0; i < len(line); i++ {
+			group[rune(line[i])] = 1
+		}
+	}
+	for _, n := range group {
+		total += n
+	}
+	fmt.Println(total)
+}
+
+func daysixparttwo(filename string) {
+	fileStream := make(chan string)
+	go files.StreamLines(filename, fileStream)
+	total := 0
+	group := make(map[rune]int)
+	groupsize := 0
+	for line := range fileStream {
+		if line == "" {
+			for _, n := range group {
+				if n == groupsize {
+					total++
+				}
+			}
+			group = make(map[rune]int)
+			groupsize = 0
+			continue
+		}
+		groupsize++
+		for i := 0; i < len(line); i++ {
+			group[rune(line[i])]++
+		}
+	}
+	for _, n := range group {
+		if n == groupsize {
+			total++
+		}
+	}
+	fmt.Println(total)
 }
