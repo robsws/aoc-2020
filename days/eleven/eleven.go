1a9a2ad071f9721bccdf4a82a73d3763bf0923b0
Rob Streeting <robsws@gmail.com>
2020-12-12 11:17:57 +0000

    reduce some code duplication in day 11

--- days/eleven/eleven.go
+++ days/eleven/eleven.go
@@ -7,28 +7,15 @@ import (
 
 // PartOne - simulate airport seating using adjacent algorithm and calculate convergent occupied seats
 func PartOne(filename string) {
-	lines := files.GetLines(filename)
-	height := len(lines)
-	width := len(lines[0])
-	grid := initGrid(width, height)
-	// Initialise the grid with the input
-	for x := 0; x < width; x++ {
-		for y := 0; y < height; y++ {
-			grid[x][y] = rune(lines[y][x])
-		}
-	}
-	// Simulate rounds until grid doesn't change
-	prevgrid := initGrid(width, height)
-	for !gridsAreEqual(grid, prevgrid) {
-		prevgrid = grid
-		grid = simulateSeats(grid, false)
-	}
-	// Count occupied seats
-	fmt.Println(countTotalOccupied(grid))
+	fmt.Println(simulateSeatsAndCountOccupied(filename, false))
 }
 
 // PartTwo - simulate airport seating using visible algorithm and calculate convergent occupied seats
 func PartTwo(filename string) {
+	fmt.Println(simulateSeatsAndCountOccupied(filename, true))
+}
+
+func simulateSeatsAndCountOccupied(filename string, useVisibleAlgorithm bool) int {
 	lines := files.GetLines(filename)
 	height := len(lines)
 	width := len(lines[0])
@@ -43,8 +30,8 @@ func PartTwo(filename string) {
 	prevgrid := initGrid(width, height)
 	for !gridsAreEqual(grid, prevgrid) {
 		prevgrid = grid
-		grid = simulateSeats(grid, true)
+		grid = simulateSeats(grid, useVisibleAlgorithm)
 	}
 	// Count occupied seats
-	fmt.Println(countTotalOccupied(grid))
+	return countTotalOccupied(grid)
 }
