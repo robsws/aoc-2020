8a3f2b83c4d26cb228bdf5664d2b3a2067b7af6b
Rob Streeting <robsws@gmail.com>
2020-12-12 11:14:27 +0000

    Input validation on the day number

--- main.go
+++ main.go
@@ -17,7 +17,10 @@ func main() {
 	if len(os.Args) > 2 {
 		test = true
 	}
-	day, _ := strconv.Atoi(daystr)
+	day, err := strconv.Atoi(daystr)
+	if err != nil || day <= 0 || day > 25 {
+		log.Fatal("Day number must be between 1 and 25 inclusive and... must actually be a number.")
+	}
 	partone, parttwo, filename := days.GetParts(day, test)
 	fmt.Println("Part one:")
 	partone(filename)
