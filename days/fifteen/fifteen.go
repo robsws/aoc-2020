5ed0114aaa4f002e40370d7478ead2c624708e29
Rob Streeting <robsws@gmail.com>
2020-12-15 08:08:16 +0000

    day 15 solutions

--- days/fifteen/fifteen.go
+++ days/fifteen/fifteen.go
@@ -1,11 +1,40 @@
 package fifteen
 
-// PartOne - not yet implemented
+import (
+	"aoc-2020/files"
+	"aoc-2020/utils"
+	"fmt"
+	"strings"
+)
+
+// PartOne - elf number game round 2020
 func PartOne(filename string) string {
-	return "Not yet implemented"
+	return fmt.Sprint(elfNumberGame(filename, 2020))
 }
 
-// PartTwo - not yet implemented
+// PartTwo - elf number game round 30 million
 func PartTwo(filename string) string {
-	return "Not yet implemented"
+	return fmt.Sprint(elfNumberGame(filename, 30000000))
+}
+
+func elfNumberGame(filename string, round int) int {
+	inputs := strings.Split(files.GetLines(filename)[0], ",")
+	lastSpoken := make(map[int]int)
+	roundResult := -1
+	for i := 0; i < round; i++ {
+		if i < len(inputs) {
+			n := utils.MustAtoi(inputs[i])
+			lastSpoken[n] = i
+			roundResult = n
+			continue
+		}
+		nLastSpoken, ok := lastSpoken[roundResult]
+		lastSpoken[roundResult] = i - 1
+		if !ok {
+			roundResult = 0
+		} else {
+			roundResult = i - 1 - nLastSpoken
+		}
+	}
+	return roundResult
 }
