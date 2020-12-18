f1227324dde562c1180584a2ffc66fc84750a027
Rob Streeting <robsws@gmail.com>
2020-12-18 10:21:05 +0000

    day 18 solutions

--- days/eighteen/eighteen.go
+++ days/eighteen/eighteen.go
@@ -1,11 +1,33 @@
 package eighteen
 
+import (
+	"aoc-2020/files"
+	"fmt"
+	"strings"
+)
+
 // PartOne - not yet implemented
 func PartOne(filename string) string {
-	return "Not yet implemented"
+	fileStream := make(chan string)
+	go files.StreamLines(filename, fileStream)
+	sum := 0
+	for line := range fileStream {
+		tokens := strings.Split(line, "")
+		result := evalLeftToRight(tokens)
+		sum += result
+	}
+	return fmt.Sprint(sum)
 }
 
 // PartTwo - not yet implemented
 func PartTwo(filename string) string {
-	return "Not yet implemented"
+	fileStream := make(chan string)
+	go files.StreamLines(filename, fileStream)
+	sum := 0
+	for line := range fileStream {
+		tokens := strings.Split(line, "")
+		result := evalAdditionFirst(tokens)
+		sum += result
+	}
+	return fmt.Sprint(sum)
 }
