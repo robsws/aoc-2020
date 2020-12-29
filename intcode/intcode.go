7f5b06bf3b9bd46aae6ddf4dd36f70780a725429
Rob Streeting <robsws@gmail.com>
2020-12-29 23:52:35 +0000

    wip: day 5 solution

--- intcode/intcode.go
+++ intcode/intcode.go
@@ -8,13 +8,28 @@ import (
 
 const opAdd = 1
 const opMultiply = 2
+const opInput = 3
+const opOutput = 4
 const opTerminate = 99
+
+var paramCount = map[int]int{
+	opAdd:       3,
+	opMultiply:  3,
+	opInput:     1,
+	opOutput:    1,
+	opTerminate: 0,
+}
+
+const positionMode = 0
+const immediateMode = 1
 
 // Program - encapsulates an intcode program
 type Program struct {
 	memory     []int
 	counter    int
-	Terminated bool
+	terminated bool
+	Input      chan int
+	Output     chan int
 }
 
 // ParseProgram - parse an intcode program from a comma separated list of ints
@@ -29,44 +44,50 @@ func ParseProgram(csv string) Program {
 
 // MakeProgram - initialize a new intcode program given initial state of memory
 func MakeProgram(initMemory []int) Program {
-	return Program{initMemory, 0, false}
+	return Program{initMemory, 0, false, make(chan int), make(chan int)}
 }
 
 // Run - run an intcode program to completion
 func (program *Program) Run() {
-	for !program.Terminated {
+	for !program.terminated {
 		program.Step()
 	}
 }
 
 // Step - run one execution cycle of an intcode program
 func (program *Program) Step() {
-	switch program.memory[program.counter] {
+	opCode, params := program.processCommand()
+	switch opCode {
 	case opAdd:
-		operandAddr1 := program.memory[program.counter+1]
-		operandAddr2 := program.memory[program.counter+2]
-		resultAddr := program.memory[program.counter+3]
-		program.add(operandAddr1, operandAddr2, resultAddr)
-		program.counter += 4
+		program.add(*params[0], *params[1], params[2])
 	case opMultiply:
-		operandAddr1 := program.memory[program.counter+1]
-		operandAddr2 := program.memory[program.counter+2]
-		resultAddr := program.memory[program.counter+3]
-		program.multiply(operandAddr1, operandAddr2, resultAddr)
-		program.counter += 4
+		program.multiply(*params[0], *params[1], params[2])
+	case opInput:
+		program.input(params[0])
+	case opOutput:
+		program.output(*params[0])
 	case opTerminate:
-		program.Terminated = true
+		program.terminated = true
 	default:
 		log.Fatal("Invalid opcode ", program.memory[program.counter])
 	}
+	program.counter += paramCount[opCode] + 1
 }
 
-func (program *Program) add(aAddr int, bAddr int, resultAddr int) {
-	program.memory[resultAddr] = program.memory[aAddr] + program.memory[bAddr]
+func (program *Program) add(a int, b int, resultPtr *int) {
+	*resultPtr = a + b
 }
 
-func (program *Program) multiply(aAddr int, bAddr int, resultAddr int) {
-	program.memory[resultAddr] = program.memory[aAddr] * program.memory[bAddr]
+func (program *Program) multiply(a int, b int, resultPtr *int) {
+	*resultPtr = a * b
+}
+
+func (program *Program) input(toPtr *int) {
+	*toPtr = <-program.Input
+}
+
+func (program *Program) output(value int) {
+	program.Output <- value
 }
 
 // Get - get a value from the program's memory
@@ -85,5 +106,36 @@ func (program *Program) Copy() Program {
 	for i, val := range program.memory {
 		newMemory[i] = val
 	}
-	return Program{newMemory, program.counter, program.Terminated}
+	return Program{newMemory, program.counter, program.terminated, make(chan int), make(chan int)}
+}
+
+func (program *Program) processCommand() (opCode int, params []*int) {
+	instruction := program.memory[program.counter]
+	opCode, paramModes := parseInstruction(instruction)
+	params = make([]*int, len(paramModes))
+	for i, mode := range paramModes {
+		paramValue := program.memory[program.counter+i+1]
+		switch mode {
+		case immediateMode:
+			params[i] = &paramValue
+		case positionMode:
+			params[i] = &program.memory[paramValue]
+		}
+	}
+	return
+}
+
+func parseInstruction(instruction int) (opCode int, paramModes []int) {
+	opCode = instruction % 100
+	opParamCount, ok := paramCount[opCode]
+	if !ok {
+		log.Fatal("Parameter count could not be obtained for ", opCode)
+	}
+	paramModes = make([]int, opParamCount)
+	instruction = (instruction - opCode) / 100
+	for i := 0; i < opParamCount; i++ {
+		paramModes[i] = instruction % 10
+		instruction = (instruction - paramModes[i]) / 10
+	}
+	return
 }
--- year2019/five/five.go
+++ year2019/five/five.go
@@ -1,8 +1,19 @@
 package five
 
+import (
+	"aoc-go/files"
+	"aoc-go/intcode"
+	"fmt"
+)
+
 // PartOne - Not yet implemented
 func PartOne(filename string) string {
-	return "Not yet implemented"
+	lines := files.GetLines(filename)
+	program := intcode.ParseProgram(lines[0])
+	go program.Run()
+	program.Input <- 1
+	result := <-program.Output
+	return fmt.Sprint(result)
 }
 
 // PartTwo - Not yet implemented
